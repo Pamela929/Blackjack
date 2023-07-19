5e98c1c8a0ddb96656b63726edba4e5de756fcbf
Pamela Aguiar Hartwig <pamela929@gmail.com>
2023-07-19 22:48:55 +0200

    Added new card function

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 let firstCard = 11
-let secondCard = 10
+let secondCard = 3
+let cards =[firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
+  renderGame()
+}
+function renderGame() {
+  cardsEl.textContent = "Cards: " + cards[0]   + " and " + cards[1]
+  sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-      message = "Do you want to draw a new card? 🙂"
+      message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-      message = "Wohoo! You've got Blackjack! 🥳"
+      message = "You've got Blackjack!"
       hasBlackJack = true
     } else {
-      message = "You're out of the game! 😭"
+      message = "You're out of the game!"
       isAlive = false
     }
 
+
     // CASH OUT!
-    console.log(message);
+    messageEl.textContent = message
+}
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    let card = 7
+    sum += card
+    renderGame()
 }
